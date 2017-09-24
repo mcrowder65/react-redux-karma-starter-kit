eaafe409ae5383cd258c31db5a6b48f0edee78e3
Matt Crowder <mcrowder65@gmail.com>
2017-09-23 23:01:05 -0500

    forgot to do npm test

--- src/client/components/navigation/header.jsx
+++ src/client/components/navigation/header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
-import Typography from "material-ui/Typography";
 import IconButton from "material-ui/IconButton";
 import MenuIcon from "material-ui-icons/Menu";
 import {connect} from "react-redux";
