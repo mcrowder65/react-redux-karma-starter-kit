8f64ab4f66ceeb832175bb8b495d9de809dcd4a3
Matt Crowder <mcrowder65@gmail.com>
2017-09-23 23:07:39 -0500

    no cd's

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd node_modules/react-redux-karma-starter-kit
 
 rm -rf ../../src
 rm -rf ../../test
@@ -9,7 +8,6 @@ cp .babelrc ../../.babelrc
 cp .eslintrc ../../.eslintrc
 cp karma.conf.js ../../karma.conf.js
 cp webpack.config.js ../../webpack.config.js
-cd ../..
 
 npm install --save html-webpack-plugin material-ui material-ui-icons prop-types react react-dom react-redux react-router react-router-dom redux webpack webpack-dev-server
 npm install --save-dev babel-core babel-eslint babel-loader babel-plugin-istanbul babel-plugin-transform-es2015-modules-umd babel-plugin-transform-object-rest-spread babel-preset-es2015 babel-preset-react chai css-loader eslint eslint-config-walmart eslint-plugin-filenames eslint-plugin-filenames eslint-plugin-jsx-a11y eslint-plugin-mocha eslint-plugin-react karma karma-babel-preprocessor karma-chai karma-chrome-launcher karma-coverage karma-mocha karma-mocha-reporter karma-sourcemap-loader karma-spec-reporter karma-webpack mocha style-loader
