449d067c70e9a291d7e88eb70e48d663018fdc3f
Matt Crowder <mcrowder65@gmail.com>
2017-09-24 10:09:48 -0500

    more descriptive

--- setup.sh
+++ setup.sh
@@ -11,7 +11,11 @@ cp webpack.config.js ../../webpack.config.js
 
 cd ../..
 echo "PWD: " $(pwd)
+echo "running npm install --save " $(node node_modules/react-redux-karma-starter-kit/list-dependencies.js)
 npm install --save $(node node_modules/react-redux-karma-starter-kit/list-dependencies.js)
+
+echo "running npm install --save-dev" $(node node_modules/react-redux-karma-starter-kit/list-dev-dependencies.js)
 npm install --save-dev $(node node_modules/react-redux-karma-starter-kit/list-dev-dependencies.js)
-#npm uninstall --save react-redux-karma-starter-kit
-#npm uninstall --save-dev react-redux-karma-starter-kit
+
+npm uninstall --save react-redux-karma-starter-kit
+npm uninstall --save-dev react-redux-karma-starter-kit
