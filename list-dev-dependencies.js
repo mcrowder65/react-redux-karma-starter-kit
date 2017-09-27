0f98aa92dac50bd427c8efca05341faabe674408
Matt Crowder <mcrowder65@gmail.com>
2017-09-27 12:39:14 -0500

    oops

--- list-dev-dependencies.js
+++ list-dev-dependencies.js
@@ -2,6 +2,6 @@
 const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("node_modules/react-redux-karma-starter-kit/package.json", "utf8"));
-const devDependencies = Object.keys(data.devDependencies).map(dep => dep + "@" + data.dependencies[dep]).join(" ");
+const devDependencies = Object.keys(data.devDependencies).map(dep => dep + "@" + data.devDependencies[dep]).join(" ");
 
 console.log(devDependencies);
