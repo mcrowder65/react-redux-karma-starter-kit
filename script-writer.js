639b19336f9acdac1a3516671a74b5030f0ea935
Matt Crowder <mcrowder65@gmail.com>
2017-11-11 17:48:08 -0600

    maybe i shouldnt give you my package.json

--- script-writer.js
+++ script-writer.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
-const pkg = require("./package.json");
+const pkg = require("../../package.json");
 pkg.scripts.start = "webpack-dev-server";
 pkg.scripts.test = "karma start";
 
-fs.writeFile("./package.json", JSON.stringify(pkg));
+fs.writeFile("../../package.json", JSON.stringify(pkg));
